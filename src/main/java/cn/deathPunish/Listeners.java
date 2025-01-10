6ce3410618062e3530bc9ed9e7ffe16f1f1ce7a4
Noogear <2364947161@qq.com>
2025-01-10 16:24:22 +0800

    Update Listeners.java

--- src/main/java/cn/deathPunish/Listeners.java
+++ src/main/java/cn/deathPunish/Listeners.java
@@ -6,31 +6,33 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Listeners implements Listener {
     private final Main plugin;
     private final Config config;
-    private final ConcurrentHashMap<UUID, Integer> foodLevel;
+    private final HashMap<UUID, Integer> foodLevel;
 
     public Listeners(Main main) {
         this.plugin = main;
         this.config = plugin.getconfig();
-        foodLevel = new ConcurrentHashMap<>();
+        foodLevel = new HashMap<>();
     }
 
 
     @EventHandler(ignoreCancelled = true)
     public void onPlayerDeath(PlayerDeathEvent event) {
-        @NotNull Player player = event.getPlayer();
-
-        foodLevel.put(player.getUniqueId(), player.getFoodLevel());
+        if (config.isKeepFoodLevel()) {
+            @NotNull Player player = event.getPlayer();
+            foodLevel.put(player.getUniqueId(), player.getFoodLevel());
+        }
 
     }
 
@@ -39,11 +41,6 @@ public class Listeners implements Listener {
         @NotNull Player player = event.getPlayer();
 
         Bukkit.getScheduler().runTaskLater(plugin, () -> {
-            if (config.isKeepFoodLevel()) {
-                if (foodLevel.containsKey(player.getUniqueId())) {
-                    player.setFoodLevel(foodLevel.get(player.getUniqueId()));
-                }
-            }
             int potionTime = config.getPotionTime();
             for (PotionEffectType potion : config.getPotionTypes()) {
                 player.addPotionEffect(new PotionEffect(potion, potionTime, config.getPotionLevel()));
@@ -56,6 +53,17 @@ public class Listeners implements Listener {
             player.sendTitle(title, subtitle, 20, 70, 20);
         }, 1);
 
+        if (config.isKeepFoodLevel()) {
+            Integer food = foodLevel.getOrDefault(player.getUniqueId(), 20);
+            if (food != 20) {
+                player.setFoodLevel(food);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        foodLevel.remove(event.getPlayer().getUniqueId());
     }
 
 }
